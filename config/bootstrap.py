9534cdc89caee157cd77cae4f2c659ed7f07031b
KozakaiAya <37061278+KozakaiAya@users.noreply.github.com>
2020-05-15 21:54:29 -0400

    update gitignore

--- config/bootstrap.py
+++ config/bootstrap.py
@@ -6,5 +6,5 @@ deluge_config['rpc_port'] = 58846
 deluge_config['username'] = 'localclient or <username>:<password>:xx in ~/.config/deluge/auth'
 deluge_config['password'] = 'some_long_password or <username>:<password>:xx in ~/.config/deluge/auth'
 
-with open('./deluge.yaml', 'w') as f:
+with open('./deluge_ex.yaml', 'w') as f:
     yaml.dump(deluge_config, f)
